c7c5e631554a67b692271911f6ba48911e4676a5
Junyi Ye <junyiye.bao@gmail.com>
2017-12-25 19:33:02 -0500

    add dropTabls.sql

---
+++ operateDatabase/operateTable.php
@@ -0,0 +1,22 @@
+<?php
+include 'connectDatabase.php';
+
+class Operation {
+	
+	public function __construct($sql) {	
+    	self::operateTable($sql);
+    }
+
+	public function operateTable($sql) {
+		//$conn;
+		try {
+		     $conn = Database::connect();
+		     $conn->exec($sql);
+		     echo ("Operation successed!\n");
+
+		} catch(PDOException $e) {
+			print_r($conn->errorInfo());
+		}
+	}
+}
+?>
