65b99929690d0d14f82079611c60c523e247c9ae
junyi ye <jy394@afsconnect1.njit.edu>
2017-12-26 08:13:43 -0500

    revise dropTables1.sql

--- operateDatabase/dropTables.php
+++ operateDatabase/dropTables.php
@@ -1,10 +1,10 @@
 <?php
 ini_set('display_errors', 'On');  
 error_reporting(E_ALL);
+
 include 'operateTable.php';
 
-
-$file_path = "dropTables.sql";
+$file_path = "dropTables1.sql";
 if(file_exists($file_path)) {
 	$fp = fopen($file_path, "r");
 	$sql = fread($fp,filesize($file_path));
