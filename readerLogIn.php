d37cd4ef2e0ae862486660b540f96ef14335dbc5
Junyi Ye <junyiye.bao@gmail.com>
2017-12-27 16:08:39 -0500

    small change on readerLogin.php

--- readerLogIn.php
+++ readerLogIn.php
@@ -10,27 +10,45 @@
 </head>
 
 <body>
-	<br>
-	<br>
-	<br>
-	<br>
-	<center><h1>The City Library System</h1></center>
-	<br>
-	<center><h2 style="color: #ff0000">Welcome Reader!</h2></center>
-	<br>
-	<br>
-	<center><p1 style="font-size: 30px">Log In</p1><center>
-	<br>
-	<br>
-	<br>
-	<form action="welcome.php" method="post">
+<?php
+// define variable readerID and set to empty values
+$readerID = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $readerID = test_input($_POST["readerID"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<br>
+<br>
+<br>
+<br>
+<center><h1>The City Library System</h1></center>
+<br>
+<center><h2 style="color: #ff0000">Welcome Reader!</h2></center>
+<br>
+<br>
+<center><p1 style="font-size: 30px">Log In</p1><center>
+<br>
+<br>
+<br>
+
+<form method="post" action="<?php echo 
+htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
 	ReaderID: <input type="text" name="readerID">
 	<input type="submit">
+</form>
 
-	<?php
-	$readerID = $_POST["readerID"]
-	echo $readerID
-	?>
-	
+<?php
+echo $readerID
+?>
+
 </body>
 </html>
